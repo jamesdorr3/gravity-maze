398c62200a96604d42bca9715e9ce04b479c4ae0
James Dorr <jamesdorr3@gmail.com>
2019-04-15 11:05:05 -0400

    games backend index, show, create

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,4 +3,25 @@ class GamesController < ApplicationController
         @games = Game.all
         render json: @games
     end
+
+    def show
+        @game = Game.find(params["id"])
+        render json: @game
+    end
+
+    def create
+        @game = Game.new(game_params)
+        if @game.save
+          render json: @game, status: :created, location: @game
+        else
+          render json: @game.errors, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def game_params
+        params.require(:game).permit(:score, :player_id)
+    end
+
 end
