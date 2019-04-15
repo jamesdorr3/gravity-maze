1e630b41d961639068ec3510b6937a5da7dc9e26
github username <github email address>
2019-04-15 11:15:04 -0400

    players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,18 @@
 class PlayersController < ApplicationController
+
     def index
         @players = Player.all
         render json: @players
+    end
+
+    def create
+      @player = Player.create(player_params)
+      render json: @player
+    end
+
+    private
+
+    def player_params
+      params.require(:player).permit(:name)
     end
 end
