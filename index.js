1117b44ffb3080cd833618e29b9572809c44be9c
github username <github email address>
2019-04-16 13:49:43 -0400

    added a second box and removed second circle

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 let x = 200;
 let y = 200;
-let a = 300;
-let b = 300;
+
 
 function setup() {
   createCanvas(400, 400);
+
 }
 
+
 function draw() {
+
   background(250);
   fill(0);
   ellipse(x,y,50,50);
+  rect(350,350,50,50);
+
 
   //x = x + 1;
 
@@ -26,22 +30,22 @@ function draw() {
   if (y <= 0) {
     y = 0;
   };
-  ellipse(a,b,30,30);
-
-  //x = x + 1;
-
-  if (a >= width){
-   a = width;
-  }
-   if (a <= 0){
-    a = 0;
-  }
-  if (b >= height) {
-    b = height;
-  }
-  if (b <= 0) {
-    b = 0;
-  }
+  // ellipse(a,b,30,30);
+  //
+  // //x = x + 1;
+  //
+  // if (a >= width){
+  //  a = width;
+  // }
+  //  if (a <= 0){
+  //   a = 0;
+  // }
+  // if (b >= height) {
+  //   b = height;
+  // }
+  // if (b <= 0) {
+  //   b = 0;
+  // }
 }
 
 function keyPressed() {
@@ -54,15 +58,16 @@ function keyPressed() {
     x = x - 50;
   } else if (keyCode === 68) {
     x = x + 50;
-  };
-  if (keyCode === UP_ARROW) {
-    b = b - 50;
-  } else if (keyCode === DOWN_ARROW) {
-   b = b + 50;
   }
-  if (keyCode === LEFT_ARROW) {
-    a = a - 50;
-  } else if (keyCode === RIGHT_ARROW) {
-    a = a + 50;
-  }
+  // if (keyCode === UP_ARROW) {
+  //   b = b - 50;
+  // } else if (keyCode === DOWN_ARROW) {
+  //  b = b + 50;
+  // }
+  // if (keyCode === LEFT_ARROW) {
+  //   a = a - 50;
+  // } else if (keyCode === RIGHT_ARROW) {
+  //   a = a + 50;
+  // }
+
 }
